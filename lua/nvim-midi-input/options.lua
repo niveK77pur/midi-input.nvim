6d0db4279402ec5100690d65970b983ac066169e
niveK77pur <kevinbiewesch@yahoo.fr>
2023-09-07 17:34:04 +0200

    feat: more robust starting logic
    - Allow providing input name using command
    - If not given, fall back to device specified in options
    - If not given, query user to select an input from a list

--- lua/nvim-midi-input/options.lua
+++ lua/nvim-midi-input/options.lua
@@ -17,4 +17,21 @@ function O.get()
     return options
 end
 
+function O.query_devices(func)
+    local choices = {}
+    for _, line in
+        ipairs(vim.fn.systemlist('lilypond-midi-input --list-devices'))
+    do
+        table.insert(choices, string.match(line, [[^[^:]+:%s*(.*)]]))
+        print(vim.inspect(choices))
+    end
+    vim.ui.select(
+        choices,
+        { prompt = 'Chose a MIDI input controller' },
+        function(choice)
+            func(choice)
+        end
+    )
+end
+
 return O
--- plugin/commands.lua
+++ plugin/commands.lua
@@ -2,9 +2,17 @@ local job = require('nvim-midi-input.job')
 local update = require('nvim-midi-input.update')
 local options = require('nvim-midi-input.options')
 
-vim.api.nvim_create_user_command('MidiInputStart', function()
-    job:start(options.get().device)
-end, { desc = 'Start MIDI Input Listener' })
+vim.api.nvim_create_user_command('MidiInputStart', function(opts)
+    if not vim.fn.empty(opts.args) then
+        job:start(opts.args)
+    elseif options.get().device then
+        job:start(options.get().device)
+    else
+        options.query_devices(function(device)
+            job:start(device)
+        end)
+    end
+end, { nargs = '?', desc = 'Start MIDI Input Listener' })
 
 vim.api.nvim_create_user_command('MidiInputStop', function()
     job:stop()
