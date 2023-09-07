782d6309effcaddf0fdc36ea610c7590485c2cba
niveK77pur <kevinbiewesch@yahoo.fr>
2023-09-07 16:48:48 +0200

    fix: properly transfer options from module

--- plugin/commands.lua
+++ plugin/commands.lua
@@ -3,7 +3,7 @@ local update = require('nvim-midi-input.update')
 local options = require('nvim-midi-input.options')
 
 vim.api.nvim_create_user_command('MidiInputStart', function()
-    job:start(options.device)
+    job:start(options.get().device)
 end, { desc = 'Start MIDI Input Listener' })
 
 vim.api.nvim_create_user_command('MidiInputStop', function()
