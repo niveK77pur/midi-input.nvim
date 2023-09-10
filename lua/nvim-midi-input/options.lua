08ca8d545157d9264b6c73088dad21d4acbec6cf
niveK77pur <kevinbiewesch@yahoo.fr>
2023-09-10 18:34:46 +0200

    refactor: get devices using dedicated function
    The output is parsed to obtain the proper names. This refactor will allow to obtain the device list more easily for use elsewhere

--- lua/nvim-midi-input/options.lua
+++ lua/nvim-midi-input/options.lua
@@ -17,15 +17,19 @@ function O.get()
     return options
 end
 
-function O.query_devices(func)
-    local choices = {}
+function O.get_devices()
+    local devices = {}
     for _, line in
         ipairs(vim.fn.systemlist('lilypond-midi-input --list-devices'))
     do
-        table.insert(choices, string.match(line, [[^[^:]+:%s*(.*)]]))
+        table.insert(devices, string.match(line, [[^[^:]+:%s*(.*)]]))
     end
+    return devices
+end
+
+function O.query_devices(func)
     vim.ui.select(
-        choices,
+        O.get_devices(),
         { prompt = 'Chose a MIDI input controller' },
         function(choice)
             func(choice)
