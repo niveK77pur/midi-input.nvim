a96124534a89df60eff372ee7cbae27c2426324f
niveK77pur <kevinbiewesch@yahoo.fr>
2023-09-10 18:35:02 +0200

    fix: properly read device name from command

--- plugin/commands.lua
+++ plugin/commands.lua
@@ -2,10 +2,19 @@ local job = require('nvim-midi-input.job')
 local update = require('nvim-midi-input.update')
 local options = require('nvim-midi-input.options')
 
+local function deviceExists(device)
+    for _, device_name in ipairs(options.get_devices()) do
+        if vim.trim(device) == vim.trim(device_name) then
+            return true
+        end
+    end
+    return false
+end
+
 vim.api.nvim_create_user_command('MidiInputStart', function(opts)
-    if not vim.fn.empty(opts.args) then
+    if vim.fn.empty(opts.args) ~= 1 and deviceExists(opts.args) then
         job:start(opts.args)
-    elseif options.get().device then
+    elseif options.get().device and deviceExists(options.get().device) then
         job:start(options.get().device)
     else
         options.query_devices(function(device)
