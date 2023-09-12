861ff5a002ec4acfe2de362d92c2bb3b6f7854fb
niveK77pur <kevinbiewesch@yahoo.fr>
2023-09-12 18:23:32 +0200

    feat: find and set last chord
    There is a limitation when it comes to multivoices. When inserting right after a multi-voice passage, the last chord will be used (from the last voice in the LilyPond file). However, this is most likely not leading to the desired results.
    Regex alone cannot provide a solution to this. We could use more elaborate vim functions to move the cursor around for example. The most ideal scenario would be a treesitter parser for LilyPond, which will make it very clear which voice is which.

--- plugin/autocommands.lua
+++ plugin/autocommands.lua
@@ -1,4 +1,5 @@
 local job = require('nvim-midi-input.job')
+local debug = require('nvim-midi-input.debug')
 
 local augroup_midideviceinput =
     vim.api.nvim_create_augroup('midideviceinput', {})
@@ -9,5 +10,42 @@ vim.api.nvim_create_autocmd({ 'ExitPre', 'QuitPre' }, {
     desc = 'Quit the MIDI Input Listener',
     callback = function()
         job:stop()
+    end,
+})
+
+vim.api.nvim_create_autocmd({ 'InsertEnter' }, {
+    group = augroup_midideviceinput,
+    pattern = { '*' },
+    desc = 'Find and set previous chord',
+    callback = function()
+        print('hello')
+        local search_pattern = [[\v\<[^>]{-}\>]]
+        local cpos = vim.api.nvim_win_get_cursor(0)
+        local e_row, e_col = unpack(vim.fn.searchpos(search_pattern, 'Wbe'))
+        local s_row, s_col = unpack(vim.fn.searchpos(search_pattern, 'nWb'))
+        if e_row == 0 and e_col == 0 then
+            -- no match was found
+            return
+        end
+        vim.api.nvim_win_set_cursor(0, cpos)
+        local chord = string.gsub(
+            vim.api.nvim_buf_get_text(
+                0,
+                s_row - 1,
+                s_col,
+                e_row - 1,
+                e_col - 1,
+                {}
+            )[1],
+            '%s+',
+            ':'
+        )
+        if debug.enabled() then
+            debug.markStartEnd(s_row - 1, s_col - 1, e_row - 1, e_col - 1)
+            print('Chord: ', chord)
+            return
+        end
+        print('Chord: ', chord)
+        job:write(string.format('previous-chord=%s', chord))
     end,
 })
