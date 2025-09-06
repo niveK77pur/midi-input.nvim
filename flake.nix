c95fb31619faba985dea4bc9c86aae2e56817933
Kevin Laurent Biewesch <kevinbiewesch@yahoo.fr>
2025-09-06 18:01:01 +0200

    build(flake): Add flake.nix for dev shell

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    utils,
+    ...
+  }:
+    utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          name = "neovim";
+          packages = with pkgs; [
+            stylua
+            lua-language-server
+            selene
+            typos
+          ];
+        };
+      }
+    );
+}
